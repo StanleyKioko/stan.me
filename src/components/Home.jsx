647a6aec14a99f9528497364f10d0bbb4adf43ce
StanleyKioko <mwangestanley02@gmail.com>
2025-07-11 23:39:57 +0300

    Added React Typed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {HiArrowRight } from 'react-icons/hi'
+import { ReactTyped } from "react-typed";
 
 const Home = () => {
   return (
@@ -9,7 +10,14 @@ const Home = () => {
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className='text-pink-600'>Hi My Name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Stanley Mwange</h1>
-        <h2 className="text-2xl sm:text-4xl font-bold text-[#8892b0]">I'm a Frontend Developer</h2>
+        <h2 className="text-2xl sm:text-4xl font-bold text-[#8892b0]">
+          <ReactTyped
+            strings={["I'm a Frontend Developer"]}
+            typeSpeed={40}
+            backSpeed={50}
+            loop
+          />
+        </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           I’m a frontend developer specializing in building exceptional digital experiences. 
           Currently, I’m focused on building responsive front-end web applications. </p>
