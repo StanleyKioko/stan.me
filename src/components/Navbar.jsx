ca3947066989dab99ff68054783807e88d4c050c
StanleyKioko <mwangestanley02@gmail.com>
2025-07-11 23:33:44 +0300

    Updated Links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,6 @@ const Navbar = () => {
             {!nav ? <FaBars /> : <FaTimes />}
         </div>
         {/*Mobile menu*/}
-        {/*Mobile menu*/}
 <ul className={!nav? 'hidden': 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center text-gray-300 md:hidden'}>
     <li className="py-6 text-4xl">
         <Link onClick={handleClick} to="home" smooth={true} duration={500}>Home</Link>
@@ -61,20 +60,20 @@ const Navbar = () => {
             <ul>
                 <li className="w-[60px] h-[60px] flex justify-between items-center ml-[-80px] hover:ml-[-10px] duration-300 bg-blue-600">
                     <a className='flex justify-between items-center'
-                    href="/">LinkedIn <FaLinkedin size={30} /></a>
+                    href="https://www.linkedin.com/in/starnley-mwange/">LinkedIn <FaLinkedin size={30} /></a>
                 </li>
                 <li className="w-[60px] h-[60px] flex justify-between items-center ml-[-70px] hover:ml-[-10px] duration-300 bg-[#333333]">
                     <a className='flex justify-between items-center'
-                    href="/">GitHub <FaGithub size={30} /></a>
+                    href="https://github.com/StanleyKioko">GitHub <FaGithub size={30} /></a>
                 </li>
-                <li className="w-[60px] h-[60px] flex justify-between items-center ml-[-60px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+                {/*<li className="w-[60px] h-[60px] flex justify-between items-center ml-[-60px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
                     <a className='flex justify-between items-center'
                     href="/">Email <HiOutlineMail size={30} /></a>
                 </li>
                 <li className="w-[60px] h-[60px] flex justify-between items-center ml-[-70px] hover:ml-[-10px] duration-300 bg-[#565f69]">
                     <a className='flex justify-between items-center'
                     href="/">Resume <BsFillPersonLinesFill size={30} /></a>
-                </li>
+                </li>*/}
             </ul>
             </div>
         </div>
